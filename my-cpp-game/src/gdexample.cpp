09a8a00e324eda88902900942aeb10f692c7e3bf
kamameshi084K <ka07zu03ki@gmail.com>
2025-12-26 21:56:21 +0900

    Initial commit

---
+++ my-cpp-game/src/gdexample.cpp
@@ -0,0 +1,31 @@
+#include "gdexample.h"
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void GDExample::_bind_methods() 
+{
+    // ここに関数やプロパティを登録するとGodotエディタから見えるようになります
+}
+
+GDExample::GDExample() 
+{
+    // コンストラクタ（初期化）
+    time_passed = 0.0;
+}
+
+GDExample::~GDExample() 
+{
+    // デストラクタ
+}
+
+void GDExample::_process(double delta) 
+{
+    // 毎フレーム呼ばれる処理
+    time_passed += delta;
+
+    // サイン波でゆらゆら動かす
+    Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 20.0)), 10.0 + (10.0 * cos(time_passed * 1.5)));
+    
+    set_position(new_position);
+}
